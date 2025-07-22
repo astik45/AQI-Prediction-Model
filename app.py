cd314ebe5676a0e4981f28aae38ddd31d7cf928b
Astik Pawar <astikpawar007@gmail.com>
2025-07-22 15:43:31 +0530

    app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import numpy as np
 import joblib
 
 # Load model
-model = joblib.load('trained_model.pkl','rb') as file:
-    model = pickle.load(file)
+model = joblib.load('trained_model.pkl','rb')
 
 st.set_page_config(page_title="AQI Predictor", layout="centered")
 st.title("🌫️ AQI Prediction App")
